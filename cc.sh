02e03176fc2eea8e200a9bcdd7eaa105442d36f4
atkurtul <atilkurtulmus@gmail.com>
2021-01-13 17:30:25 +0300

    Create cc.sh

---
+++ cc.sh
@@ -0,0 +1,15 @@
+
+./configure --with-features=huge \
+  --enable-multibyte \
+  --enable-cscope \
+  --prefix=/usr \
+  --enable-fontset \
+  --disable-gui \
+  --enable-cscope \
+  --enable-luainterp=yes \
+  --enable-pythoninterp=yes \
+  --enable-python3interp=yes \
+  --prefix=/usr \
+  --with-lua-prefix=/usr \
+  --with-luajit \
+  --enable-perlinterp
