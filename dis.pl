1c22df8187dce34a93ef1f3ad04e203acf4f72c1
atkurtul <atilkurtulmus@gmail.com>
2021-01-19 00:45:05 +0300

    Update dis.pl

--- dis.pl
+++ dis.pl
@@ -3,10 +3,12 @@
 $asm = `objdump -d -Mintel @ARGV[0]`;
 
 if (@ARGV >= 2) {
-  if ("$asm\n" =~ /(\d+ \<@ARGV[1]\>:(.|\n)*?)\n\n/g) {
-    print "$1\n";
-  } else {
-    print "No match for <@ARGV[1]>\n";
+  foreach $arg (@ARGV[1..@ARGV-1]) {
+    if ("$asm\n" =~ /(\d+ \<$arg\>:(.|\n)*?)\n\n/g) {
+      print "$1\n";
+    } else {
+      print "No match for <$arg>\n";
+    }
   }
 } else {
   print "$asm\n";
