5bfdc8df1fb22860205f1f260bb22ecf0509c455
atil <atilkurtulmus@gmail.com>
2021-01-23 01:26:38 +0300

    push

--- alacritty.sh
+++ alacritty.sh
@@ -5,3 +5,5 @@ sudo apt install curl
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo apt install libxcb1-dev libxcb-render0-dev libxcb-xfixes0-dev libxcb-shape0-dev libfontconfig-dev
 cargo install alacritty
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator `which alacritty` 100
