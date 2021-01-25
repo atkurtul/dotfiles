3615ff94c5f014f0bd7d578f19022e2c59d6c1c6
atil <atilkurtulmus@gmail.com>
2021-01-25 12:19:35 +0300

    push

---
+++ cc.sh
@@ -0,0 +1,17 @@
+
+LDFLAGS=-L/usr/lib/x86_64-linux-gnu
+
+./configure --with-features=huge \
+  --enable-multibyte \
+  --enable-cscope \
+  --prefix=/usr \
+  --enable-fontset \
+  --disable-gui \
+  --enable-cscope \
+  --enable-luainterp=yes \
+  --enable-python3interp=yes \
+  --prefix=/usr \
+  --with-lua-prefix=/usr \
+  --enable-perlinterp \
+  --enable-fail-if-missing \
+  --with-python3-config-dir=/usr
