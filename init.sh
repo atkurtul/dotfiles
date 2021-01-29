c3afe3930a30494f85ec8584fedf137a991c918c
atkurtul <atilkurtulmus@gmail.com>
2021-01-29 12:00:31 +0300

    push

---
+++ init.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+alias q=exit
+alias modx='chmod +x'
+
+export LS_COLORS="$LS_COLORS:ow=0;34:"
+export PATH="$PATH:~/.local/bin:~/dotfiles/scripts"
+
+
+export VISUAL=nvim
+export EDITOR=nvim
+
+function pushfast() {
+  if [[ -z $1 ]]; then
+  	msg="push"
+  else
+    msg="$1"
+  fi
+  git add .
+  git commit -m $msg
+  git push
+}
+
+function dumpcolor() {
+  for x in {0..8}; do 
+      for i in {30..37}; do 
+          for a in {40..47}; do 
+              echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+          done
+          echo
+      done
+  done
+  echo ""
+}
+
